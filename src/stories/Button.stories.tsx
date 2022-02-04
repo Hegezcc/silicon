b6ddb58e9f76c9c874a0a9e5bc415ecadf086b97
Taufik Pragusga <taufik@pragusga.com>
2022-02-04 14:21:41 +0700

    fix: fix broken style in loading state

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -111,3 +111,43 @@ Full.args = {
   size: 'full',
   text: 'Full',
 }
+
+export const LeftIcon = Default.bind({})
+LeftIcon.args = {
+  variant: 'primary',
+  text: 'Button',
+  leftIcon: (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M19.4753 11.29L14.8953 6.70001L16.3053 5.29001L22.3053 11.29L18.7153 14.88L17.3053 13.46L19.4753 11.29ZM1.69531 3.51001L5.88531 7.70001L2.30531 11.29L8.30531 17.29L9.71531 15.88L5.13531 11.29L7.30531 9.12001L20.0853 21.9L21.4953 20.49L3.11531 2.10001L1.69531 3.51001Z"
+        fill="white"
+      />
+    </svg>
+  ),
+}
+
+export const MidIcon = Default.bind({})
+MidIcon.args = {
+  variant: 'primary',
+  midIcon: (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M19.4753 11.29L14.8953 6.70001L16.3053 5.29001L22.3053 11.29L18.7153 14.88L17.3053 13.46L19.4753 11.29ZM1.69531 3.51001L5.88531 7.70001L2.30531 11.29L8.30531 17.29L9.71531 15.88L5.13531 11.29L7.30531 9.12001L20.0853 21.9L21.4953 20.49L3.11531 2.10001L1.69531 3.51001Z"
+        fill="white"
+      />
+    </svg>
+  ),
+}
+export const RightIcon = Default.bind({})
+RightIcon.args = {
+  variant: 'primary',
+  text: 'Button',
+  rightIcon: (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M19.4753 11.29L14.8953 6.70001L16.3053 5.29001L22.3053 11.29L18.7153 14.88L17.3053 13.46L19.4753 11.29ZM1.69531 3.51001L5.88531 7.70001L2.30531 11.29L8.30531 17.29L9.71531 15.88L5.13531 11.29L7.30531 9.12001L20.0853 21.9L21.4953 20.49L3.11531 2.10001L1.69531 3.51001Z"
+        fill="white"
+      />
+    </svg>
+  ),
+}
